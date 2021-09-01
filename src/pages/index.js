1a95ca9dc371e38ba4061572606c5ead6538bbb0
Trevor Knutson <trevor.a.knutson@ndsu.edu>
2021-09-01 16:34:39 -0500

    Finished Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import { Link } from 'gatsby';
+import { StaticImage } from 'gatsby-plugin-image';
 import * as React from 'react';
 import Layout from '../components/layout';
 
@@ -6,6 +7,10 @@ const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this site following the Gabsby tutorial!</p>
+      <StaticImage 
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera" 
+        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+      />
     </Layout>
   );
 }
